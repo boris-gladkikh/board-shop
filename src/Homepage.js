99705f5a4919ece3f62fa6557e25c777cbe5a961
gladkill <gladkillmusic@gmail.com>
2020-09-10 14:51:29 -0700

    made header font and image responsive and stylized on home page, included google fonts and header styling across doc

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './Homepage.css';
 import CompanyBox from './CompanyBox';
+import downhillSkateboarder from './image/electric_skateboard_hill.jpg';
+import groupSkating from './image/group-skating.jpg'
 
-function Homepage(){
-  return(
+function Homepage() {
+  return (
     <div className="App">
-
-      <div className="p-4">
-      <h2>Welcome To FlowState Boards.</h2>
-      <h5>Your one stop shop for cutting-edge electric skateboards.</h5>
+      <div className="header-box">
+        <div className="header-text">
+          <h1>FLOWSTATE BOARDS</h1>
+          <h6>Your one stop shop for electric skateboards & accessories</h6>
+        </div>
+        <img className="header-image" src={groupSkating} alt="group skating in SF"></img>
       </div>
+
+
 
       <div className="home-box p-4">
         <h4>ABOUT US:</h4>
         <hr></hr>
-        <p className="px-4">
+        <p className="px-3 w-75 m-auto">
           We are a small, dedicated team of skateboarders, engineers and electric vehicle enthusiasts dedicated
           to bringing you the most cutting-edge electric boards from the most trusted names.
           We aim to provide an honest price, thoughtful customer service, and a lasting relationship
-          with our featured brands and customers. 
+          with our featured brands and customers.
         </p>
       </div>
 
