a7dd9ce042c88ede834f6e18ffff1d2ad6bad43e
gladkill <gladkillmusic@gmail.com>
2020-09-10 17:46:35 -0700

    added quotes to reviews, added logos to company box

--- src/CompanyBox.js
+++ src/CompanyBox.js
@@ -2,12 +2,27 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import './CompanyBox.css';
 
 function CompanyBox(){
   return(
     <Container>
-      <Row>
-        <Col sm="12" md="6"></Col>
+      <Row className="m-auto"> 
+        <Col sm="12" md="6" lg="3">
+          <img className="logo img-fluid" src="img/onewheel-logo.jpg" alt="onewheel logo"></img>
+        </Col>
+        <Col sm="12" md="6" lg="3">
+          <img className="logo img-fluid" src="img/meepo-logo.png" alt="meepo logo"></img>
+        </Col>
+        <Col sm="12" md="6" lg="3">
+          <img className="logo img-fluid" src="img/evolve-logo.jpg" alt="evolve logo"></img>
+        </Col>
+        <Col sm="12" md="6" lg="3">
+          <img className="logo img-fluid" src="img/ownboard-logo.jpg" alt="ownboard logo"></img>
+        </Col>
+        <Col sm="12" md="6" lg="3">
+          <img className="logo img-fluid" src="img/boosted-logo.png" alt="boosted logo"></img>
+        </Col>
 
       </Row>
     </Container>
