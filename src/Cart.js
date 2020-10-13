2f4e879f50bb3523cffab357b84e7f08a93db36a
gladkill <gladkillmusic@gmail.com>
2020-10-12 18:50:46 -0700

    added empty initial state to rootreducer, added conditional if cart is empty to cart component

--- src/Cart.js
+++ src/Cart.js
@@ -1,56 +1,69 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import './Cart.css';
-import CartItem from './CartItem';
-import {useSelector} from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import "./Cart.css";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-
-// cart page, displaying cart item components 
-// for items that are currently inside cart store.
+/**
+ * TODO: make cart responsive (breaks on smaller screens)
+ * add subtotal to cart, showing subtotal of all
+ * cart page, displaying cart item components for items that are currently inside cart store.
+ *
+ */
 
 function Cart() {
-  const shoppingCart = useSelector(store => store.cart);
+  const shoppingCart = useSelector((store) => store.cart);
   const history = useHistory();
 
-  const shoppingList = shoppingCart.map(p => 
-    (
-    <Col lg="12" >
-      <CartItem 
-      key={p.id}
-      id={p.id}
-      name={p.name}
-      price={p.price}
-      img={p.img}
-      qty={p.qty} />
+  const shoppingList = shoppingCart.map((p) => (
+    <Col lg="12">
+      <CartItem
+        key={p.id}
+        id={p.id}
+        name={p.name}
+        price={p.price}
+        img={p.img}
+        qty={p.qty}
+      />
     </Col>
-  ))
+  ));
 
-  function keepShopping(){
-    history.push("/shop")
+  let cartDisplay =
+    shoppingCart.length > 0 ? (
+      <Row>{shoppingList}</Row>
+    ) : (
+      <div>
+        {" "}
+        <h4 className="text-white">Shopping Cart is Empty!</h4>
+      </div>
+    );
+
+  function keepShopping() {
+    history.push("/shop");
   }
 
-  function checkOut(){
-    history.push("/checkout")
+  function checkOut() {
+    history.push("/checkout");
   }
-
 
   return (
     <div className="App">
       <h2 className="mt-5">MY CART</h2>
       <div className="cart">
-        <Row>
-          {shoppingList}
-        </Row>
+        {cartDisplay}
         <div className="text-right mt-3">
-          <Button onClick={keepShopping} variant="dark" className="mx-1">Keep Shopping</Button>
-          <Button onClick={checkOut} variant="dark">Checkout</Button>
+          <Button onClick={keepShopping} variant="dark" className="mx-1">
+            Keep Shopping
+          </Button>
+          <Button onClick={checkOut} variant="dark">
+            Checkout
+          </Button>
         </div>
       </div>
     </div>
-  )
-
+  );
 }
 export default Cart;
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,20 +1,26 @@
+// const INITIAL_STATE = {
+//   cart: [{
+//     id: "ownat1w",
+//     name: "Ownboard AT1W",
+//     brand: "Ownboard",
+//     price: "799.00",
+//     img: "/img/boards/ownboard-at1w.jpg",
+//     qty: 1
+//   }, {
+//     id: "meeponlspro",
+//     name: "Meepo NLS Pro",
+//     brand: "Meepo",
+//     price: "689.00",
+//     img: "/img/boards/meepo-nls-pro.jpg",
+//     qty: 1
+//   }],
+//   subtotal: 1488.00
+// };
+
 const INITIAL_STATE = {
-  cart: [{
-    id: "ownat1w",
-    name: "Ownboard AT1W",
-    brand: "Ownboard",
-    price: "799.00",
-    img: "/img/boards/ownboard-at1w.jpg",
-    qty: 1
-  }, {
-    id: "meeponlspro",
-    name: "Meepo NLS Pro",
-    brand: "Meepo",
-    price: "689.00",
-    img: "/img/boards/meepo-nls-pro.jpg",
-    qty: 1
-  }]
-};
+  cart: [],
+  subtotal: 0
+}
 
 //initial state holds cart with array of item objects - hardcoded initial state for now
 
