901bd76fb2a91bcb47c076770e27f5e110e89a9c
gladkill <gladkillmusic@gmail.com>
2020-10-12 18:37:11 -0700

    planned checkout flow and renamed checkout form. added hover effect to productcard and link to product detail.

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+/*
+* TODO::
+* create checkout page displaying shipping form/billing form
+* create credit card form that shows after shipping/billing complete
+* create checkoutAfter page that shows order being complete & confetti or something?
+* show cart in itemized list (pull card from redux store)
+* create subtotal/ tax / shipping / 
+*/
+
+
 function Checkout(){
   return(
     <div className="App">
 
       <h1>checkout!</h1>
+      
     </div>
   )
 }
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import {NavLink} from 'react-router-dom';
+import './ProductCard.css';
 
 function ProductCard({product}){
   return(
     <Card className="m-2">
-      <img className="img-fluid" src={product.img} alt={`board detail for ${product.name}`}></img>
+      <NavLink to={`/shop/${product.name}`}>
+      <img className="img-fluid img-link" src={product.img} alt={`board detail for ${product.name}`}></img>
+      </NavLink>
       <p><b>{product.name}</b></p>
       <p><b>By {product.brand} - ${product.price} </b></p>
       <Button variant="dark">Add To Cart</Button>
