494aeafeb1b532da24aae26b5c3b0410780d1f7d
gladkill <gladkillmusic@gmail.com>
2020-10-16 17:02:43 -0700

    installed bootstrap dependency for compile error on netlify deploy

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import rootReducer from './rootReducer';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
